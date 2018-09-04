1838a379015ea681e6ef331ddb2bc3ed4d39f861
nicholas-kaloger <njk.kaloger@gmail.com>
2018-09-04 14:01:53 +1000

    Style changes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./app.css";
+import "./components/styles/app.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="row">
+        <div id="game-container">
+          <p className="subtext">Favourite Game</p>
+          <h3> No Game Chosen</h3>
+        </div>
+        <div id="goal-container">
+          <p>
+            <span id="favourite-game">Game Library</span>
+          </p>
+        </div>
+        <div id="right-container">
+          <button id="add-button">Add New Game</button>
+        </div>
       </div>
     );
   }
